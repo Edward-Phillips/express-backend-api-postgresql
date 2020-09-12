a58c838b57eb68b5f6a926bb8e1c2501621d3f4e
Edward Phillips <edd0ddi0@gmail.com>
2020-09-12 18:47:13 +0100

    linting, remove empty newlines

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
+
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
