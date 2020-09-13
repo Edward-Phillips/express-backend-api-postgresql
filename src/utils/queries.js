05e1ee669cdca7b5cdabab10aad5d5656adb7966
Edward Phillips <edd0ddi0@gmail.com>
2020-09-13 15:27:51 +0100

    add products test queries

---
+++ src/utils/queries.js
@@ -0,0 +1,17 @@
+export const createProductsTable = `
+DROP TABLE IF EXISTS products;
+CREATE TABLE IF NOT EXISTS products(product_id serial PRIMARY KEY,
+  product_name VARCHAR ( 255 ) NOT NULL,
+  price NUMERIC,
+  cutting_type VARCHAR ( 255 ) NOT NULL
+)
+`;
+
+export const insertProducts = `
+INSERT INTO products(product_name, price, cutting_type)
+VALUES
+  ('Philodendron Squamiferum', 3, 'rooted'),
+  ('Philodendron Squamiferum', 1.5, 'unrooted')
+`;
+
+export const dropProductsTable = 'DROP TABLE products';
