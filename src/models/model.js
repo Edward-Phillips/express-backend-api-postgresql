eaa05ed5a99aed489deef4c2cf5dff1bbfb60444
Edward Phillips <edd0ddi0@gmail.com>
2020-09-13 20:21:12 +0100

    add insertWithReturn method to model class

--- src/models/model.js
+++ src/models/model.js
@@ -14,6 +14,15 @@ class Model {
     let query = `SELECT ${columns} FROM ${this.table}`;
     if (clause) query += clause;
     return this.pool.query(query);
+  };
+
+  async insertWithReturn(columns, values) {
+    const query = `
+      INSERT INTO ${this.table}(${columns})
+      VALUES (${values})
+      RETURNING id, ${columns}
+      `;
+    return this.pool.query(query);
   }
 }
 
