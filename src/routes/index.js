eb27207a97d096155266dcffd2bdf75c54f32915
Edward Phillips <edd0ddi0@gmail.com>
2020-09-13 16:03:54 +0100

    add route for /products

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { indexPage, stockPage } from '../controllers';
+import { indexPage, stockPage, productsPage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/stock', stockPage);
+indexRouter.get('/products', productsPage);
 
 export default indexRouter;
