55d57574ef7e04b9817112061fde8fb5aa557812
Edward Phillips <edd0ddi0@gmail.com>
2020-09-13 20:20:49 +0100

    add products post controller method

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -11,3 +11,15 @@ export const productsPage = async (req, res) => {
     res.status(200).json({ products: err.stack });
   }
 };
+
+export const addProduct = async (req, res) => {
+  const { product_name, price, cutting_type } = req.body;
+  const columns = 'product_name, price, cutting_type';
+  const values = `'${product_name}', ${price}, '${cutting_type}'`;
+  try {
+    const data = await productsModel.insertWithReturn(columns, values);
+    res.status(200).json({ products: data.rows });
+  } catch (err) {
+    res.status(200).json({ products: err.stack });
+  }
+};
