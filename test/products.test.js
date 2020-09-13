ccc514f589e11667499ded57e5034b260fa5259b
Edward Phillips <edd0ddi0@gmail.com>
2020-09-13 16:18:21 +0100

    add done method as parameter to it()

--- test/products.test.js
+++ test/products.test.js
@@ -1,14 +1,14 @@
 import { expect, server, BASE_URL } from './setup';
 
 describe('Products', () => {
-  it('get products page', () => {
+  it('get products page', (done) => {
     server
       .get(`${BASE_URL}/products`)
       .expect(200)
       .end((err, res) => {
-        expect(res.tatus).to.equal(200);
-        expect(res.body.messages).to.be.instanceOf(Array);
-        res.body.messages.forEach((m) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.products).to.be.instanceOf(Array);
+        res.body.products.forEach((m) => {
           expect(m).to.have.property('product_name');
           expect(m).to.have.property('price');
           expect(m).to.have.property('cutting_type');
