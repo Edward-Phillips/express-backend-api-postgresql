198d81abc3cac66a5aeca9c34955f3e75b4bb0f1
Edward Phillips <59967673+Edward-Phillips@users.noreply.github.com>
2020-09-14 14:15:17 +0100

    remove middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { indexPage, stockPage, productsPage, addProduct } from '../controllers';
-import { modifyProduct } from '../middleware';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/stock', stockPage);
 indexRouter.get('/products', productsPage);
-indexRouter.post('/products', modifyProduct, addProduct);
+indexRouter.post('/products', addProduct);
 
 export default indexRouter;
